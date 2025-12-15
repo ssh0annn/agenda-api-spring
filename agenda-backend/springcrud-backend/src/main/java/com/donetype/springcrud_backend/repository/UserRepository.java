1e9cfd77b6d9d43d40b2ee61fc6cb94dfdf76c09
Johan Diaz <158389925+ssh0annn@users.noreply.github.com>
2025-12-15 00:20:15 -0400

    Delete agenda-backend directory

--- agenda-backend/springcrud-backend/src/main/java/com/donetype/springcrud_backend/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.donetype.springcrud_backend.repository;
-
-import com.donetype.springcrud_backend.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Long> {
-}

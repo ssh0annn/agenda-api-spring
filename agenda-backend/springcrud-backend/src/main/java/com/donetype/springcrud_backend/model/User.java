2525f98b7b3ec9545ddbf76d74c9002bca1abe9e
Johan Diaz <158389925+ssh0annn@users.noreply.github.com>
2025-12-15 00:25:28 -0400

    Back agregado correctamente

---
+++ agenda-backend/springcrud-backend/src/main/java/com/donetype/springcrud_backend/model/User.java
@@ -0,0 +1,49 @@
+package com.donetype.springcrud_backend.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String name;
+    private String email;
+
+    // Getters y Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
